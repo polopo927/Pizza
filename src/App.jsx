829f5b9fcee8fa7754a1e2e1bd621c8be7d36ba1
polopo927 <polopo927@mail.ru>
2024-07-17 14:10:25 +0300

    фетч запрос пицц

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,20 @@ import { Header } from './components/Header';
 import { PizzaCard } from './components/PizzaCard';
 import { Sort } from './components/Sort';
 import './scss/app.scss'
-import pizzas from './assets/pizzas.json'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+    fetch('https://66965e360312447373c2421d.mockapi.io/pizzas')
+      .then(res => res.json()) // приходит массив arr
+      .then((arr) => {
+        setPizzas(arr)
+        console.log(arr)
+      })
+  }, [])
+
   return (
     <div className="wrapper">
       <Header />
