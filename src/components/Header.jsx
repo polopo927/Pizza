e9b8d4cccbb3a231a21f99eea46e9e56bc6e4170
polopo927 <polopo927@mail.ru>
2024-07-23 12:04:44 +0300

    пагинация

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
+import { Search } from "./Search"
 
-export const Header = () => {
+export const Header = ({ searchValue, setSearchValue }) => {
   return (
     <div className="header">
       <div className="container">
@@ -13,6 +14,7 @@ export const Header = () => {
             </div>
           </Link>
         </div>
+        <Search searchValue={searchValue} setSearchValue={setSearchValue} />
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
             <span>520 ₽</span>
