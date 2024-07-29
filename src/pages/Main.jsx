4df1b4e4288f5f829d931177d26b9044a722ee26
polopo927 <polopo927@mail.ru>
2024-07-29 19:44:48 +0300

    debounce

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
+import axios from 'axios'
 import { Categories } from "../components/Categories";
 import { Sort } from "../components/Sort";
 import { PizzaCard } from "../components/pizzaCard";
@@ -7,47 +8,60 @@ import { Pagination } from "../pagination";
 import { SearchContext } from "../App";
 
 import { useSelector, useDispatch } from 'react-redux'
-import { setCategoryId } from '../redux/slices/filterSlice'
+import { setCategoryId, setCurrentPage } from '../redux/slices/filterSlice'
 
 export const Main = () => {
   const { searchValue } = useContext(SearchContext)
   const [pizzas, setPizzas] = useState([])
   const [isLoading, setIsLoading] = useState(true)
-  const [currentPage, setCurrentPage] = useState(1)
+  const [isError, setIsError] = useState(false)
 
   const dispatch = useDispatch()
-  const { categoryId, sort } = useSelector(state => state.filter)
+  const { categoryId, sort, currentPage } = useSelector(state => state.filter)
   const sortType = sort.sortProperty
 
-  const url = `https://66965e360312447373c2421d.mockapi.io/pizzas?page=${currentPage}&limit=4&`
-  const category = categoryId > 0 ? `category=${categoryId}` : ''
-  const search = searchValue ? `&search=${searchValue}` : ''
-  const sortBy = `&sortBy=${sortType}&order=desc`
+
 
   const onChangeCategory = (id) => {
     dispatch(setCategoryId(id))
   }
 
+  const onChangePage = number => {
+    dispatch(setCurrentPage(number))
+  }
+
+
+  const url = `https://66965e360312447373c2421d.mockapi.io/pizzas?page=${currentPage}&limit=4`
+  const category = categoryId > 0 ? `category=${categoryId}` : ''
+  const search = searchValue ? `&search=${searchValue}` : ''
+  const sortBy = `&sortBy=${sortType}&order=desc`
 
   useEffect(() => {
     setIsLoading(true)
-    fetch(`${url}${category}${sortBy}${search}`)
-      .then(res => res.json()) // приходит массив arr
-      .then((data) => {
-        if (Array.isArray(data)) {
-          setPizzas(data)
-        } else {
-          setPizzas([])
-        }
+    setIsError(false)
+      axios
+      .get(`${url}${category}${sortBy}${search}`)
+      .then(response => {
+        setPizzas(response.data)
         setIsLoading(false)
+      })
+      .catch(error => {
+        if (error.response && error.response.status === 404) {
+          setIsError(true)
+          setIsLoading(false)
+        } else {
+          console.log('Произошла ошибка, пицца не найдена', error)
+        }
       })
     window.scroll(0, 0)
     // eslint-disable-next-line
   }, [categoryId, sortType, searchValue, currentPage])
 
-  const items = pizzas.length > 0
-    ? pizzas.map(pizza => <PizzaCard key={pizza.id} {...pizza} />)
-    : <p>Пиццы не найдено</p>
+  const items = isError 
+  ? <p>Пиццы не найдено</p> 
+  : pizzas.length > 0 
+    ? pizzas.map(pizza => <PizzaCard key={pizza.id} {...pizza} />) 
+    : null;
 
   const skeletons = [...new Array(8)].map((_, index) => <Skeleton key={index} />)
 
@@ -58,9 +72,7 @@ export const Main = () => {
           categoryId={categoryId}
           onChangeCategory={onChangeCategory}
         />
-        <Sort
-          
-        />
+        <Sort/>
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
@@ -69,7 +81,7 @@ export const Main = () => {
           : items
         }
       </div>
-      <Pagination onChangePage={number => setCurrentPage(number)} />
+      <Pagination currentPage={currentPage} onChangePage={onChangePage} />
     </div>
   )
 }
